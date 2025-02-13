c9462fc24878693208fcef63ef78e8f57bb0aa34
matheusbatista103 <162481378+matheusbatista103@users.noreply.github.com>
2025-02-13 18:24:18 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi 2F, sou o Matheus</p>
+
+    <?php
+        $nome = "Matheus";
+        echo "<p>Oi 2F, sou o Matheus";
+        echo "<p>Oi 2F, sou o $nome</p>";
+        echo "<p>Oi 2F, sou o ".$nome." </p>";
+    ?>
+</body>
+</html>
---
+++ aula02/exemplo02.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 36;
+    $b = "amendoim 11";
+    $s = $a + $b;
+
+    echo $s;
+
+
+?>
