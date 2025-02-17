0fce3c19f112661f7121664f21e1e3f29ab94ead
matheusbatista103 <162481378+matheusbatista103@users.noreply.github.com>
2025-02-17 14:54:43 -0300

    Add files via upload

---
+++ aula02/exemplo04.php
@@ -0,0 +1,8 @@
+<?php
+    define('matheus',1.5);
+    $a = 1.5;
+
+    echo "Valor: ".matheus;
+    echo "<br>";
+    echo "Valor: $a";
+?>
---
+++ aula02/exemplo06.php
@@ -0,0 +1,14 @@
+<?php
+    $nome = "matheus";
+    $idade = "15";
+    $funcao = "estudante";
+
+    $frase = "Dados pessoais: ";
+    $frase .= $nome;
+    $frase .= " - ";
+    $frase .= $idade;
+    $frase .= " - ";
+    $frase .= $funcao;
+
+    echo $frase;
+?>
