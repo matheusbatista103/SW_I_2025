e259f0809a2af2c39d30220f207b2969e01b65d9
matheusbatista103 <162481378+matheusbatista103@users.noreply.github.com>
2025-06-25 14:26:02 -0300

    Update retangulo.php

--- aula13/exercicio04/retangulo.php
+++ aula13/exercicio04/retangulo.php
@@ -1,6 +1,12 @@
 <?php
-require_once "Retangulo.php";
+include("retangulo.class.php");
 
-$ret = new Retangulo(5, 3);
-$ret->exibirDados();
+$ret = new Retangulo();
+$ret->setLargura(5);
+$ret->setAltura(10);
+
+echo "Largura: " . $ret->getLargura() . "<br>";
+echo "Altura: " . $ret->getAltura() . "<br>";
+echo "Área: " . $ret->calcularArea() . "<br>";
+echo "Perímetro: " . $ret->calcularPerimetro();
 ?>
